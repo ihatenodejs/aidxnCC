6b0a1c64e13943ec5e0886481ef9a113bad37adc
Aidan <aidan@p0ntus.com>
2025-01-06 01:23:00 -0500

    add link to blog

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWordpressSimple } from '@fortawesome/free-brands-svg-icons'
 import { faLink, faHouse, faUser, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
@@ -11,6 +12,7 @@ export default function Header() {
           <li><Link href="/about" className="flex flex-col sm:flex-row items-center text-gray-300 hover:text-white transition-colors"><FontAwesomeIcon icon={faUser} className="text-md mr-2" /> About</Link></li>
           <li><Link href="/contact" className="flex flex-col sm:flex-row items-center text-gray-300 hover:text-white transition-colors"><FontAwesomeIcon icon={faPhone} className="text-md mr-2" /> Contact</Link></li>
           <li><Link href="/domains" className="flex flex-col sm:flex-row items-center text-gray-300 hover:text-white transition-colors"><FontAwesomeIcon icon={faLink} className="text-md mr-2" /> Domains</Link></li>
+          <li><Link href="https://blog.aidxn.fun/" className="flex flex-col sm:flex-row items-center text-gray-300 hover:text-white transition-colors"><FontAwesomeIcon icon={faWordpressSimple} className="text-md mr-2" /> Blog</Link></li>
         </ul>
       </nav>
     </header>
