2c997c8e8113829f2f53c7ad31d1ba9851ec8d3f
Aidan <aidan@p0ntus.com>
2025-01-06 13:08:40 -0500

    reduce amount of space taken by header in md screen size

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
             <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className="text-2xl" />
           </button>
         </div>
-        <ul className={`flex-col md:flex-row md:flex space-x-0 md:space-x-12 absolute md:static bg-gray-800 md:bg-transparent w-full md:w-auto left-0 md:left-auto top-16 md:top-auto transition-all duration-300 ease-in-out ${isOpen ? 'flex' : 'hidden'}`}>
+        <ul className={`flex-col md:flex-row md:flex space-x-0 md:space-x-3 absolute md:static bg-gray-800 md:bg-transparent w-full md:w-auto left-0 md:left-auto top-16 md:top-auto transition-all duration-300 ease-in-out ${isOpen ? 'flex' : 'hidden'}`}>
           <li><Link href="/" className="flex items-center text-gray-300 hover:text-white hover:bg-gray-700 rounded-md px-3 py-2 transition-all"><FontAwesomeIcon icon={faHouse} className="text-md mr-2" /> Home</Link></li>
           <li><Link href="/about" className="flex items-center text-gray-300 hover:text-white hover:bg-gray-700 rounded-md px-3 py-2 transition-all"><FontAwesomeIcon icon={faUser} className="text-md mr-2" /> About</Link></li>
           <li><Link href="/contact" className="flex items-center text-gray-300 hover:text-white hover:bg-gray-700 rounded-md px-3 py-2 transition-all"><FontAwesomeIcon icon={faPhone} className="text-md mr-2" /> Contact</Link></li>
