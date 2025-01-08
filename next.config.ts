28fc3ea7d82a7e99f806ed5c6626a3336e52b831
Aidan <aidan@p0ntus.com>
2025-01-08 08:41:44 -0500

    add readme stats to next image conf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lastfm.freetls.fastly.net', 'p0ntus.com'],
+    domains: ['lastfm.freetls.fastly.net', 'p0ntus.com', 'github-readme-stats.vercel.app'],
   },
 };
 
